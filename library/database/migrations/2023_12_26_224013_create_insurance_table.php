2ed982fb029d24086c33a3f382a04d1402e633d7
DavidkasUnit <94062144+DavidkasUnit@users.noreply.github.com>
2023-12-27 09:47:26 +0200

    init
    upload project

---
+++ library/database/migrations/2023_12_26_224013_create_insurance_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('insurance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('property_id')->constrained('properties');
+            $table->string('insurance_type');
+            $table->decimal('amount');
+            $table->dateTime('valid_until');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('insurance');
+    }
+};
